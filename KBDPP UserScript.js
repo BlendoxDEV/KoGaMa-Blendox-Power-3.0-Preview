e9ce49b78443bb2ac479c0a471d6a38af805a276
Androider DEV <125046976+Androiderjs@users.noreply.github.com>
2023-05-22 14:04:58 -0300

    Create KBDPP UserScript.js
    Preview 1.0.0

---
+++ KBDPP UserScript.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         KoGaMa Blendox Power 3.0 Preview
+// @namespace    https://sites.google.com/view/blendox
+// @version      3.0
+// @description  Preview Version
+// @author       Blend and HenryPierre
+// @match        https://kogama.com.br/
+// @match        https://kogama.com.br/games/
+// @match        https://kogama.com.br/page/webgl-frame/
+// @match        https://www.kogama.com/page/webgl-frame/
+// @match        https://friends.kogama.com/page/webgl-frame/
+// @icon         https://img.icons8.com/?size=512&id=fk75RbGHNtqW&format=png
+// @grant        none
+// ==/UserScript==
+window.alert("KoGaMa Blendox Power 3.0 Preview Online.");
+console.log("Made By NetherDEV And HenryPierre");
+var iconStart = querySelector(icon-menu);
+function Ativado(){
+const menu = document.createElement("div");
+iconStart.addEventListener("oncontextmenu", function(){
+//menu.style.position = "sticky";
+//menu.style.top = "78%";
+//menu.style.left = "50%";
+menu.style.width = "100px"
+menu.style.lenght = "100px"
+//menu.style.transform = "translate(-50%, -50%)";
+menu.style.backgroundColor = "Transparent";
+menu.style.border = "5px relative red";
+menu.style.padding = "20px";
+})
+}
+
+websocketsend
+
+/*
+document.body.appendChild(menu);
+    const button1 = top.document.createElement('button');
+    button1.innerText = "Crash"
+    top.document.body.appendChild(Button1);
+const button2 = top.document.createElement('button');
+    button2.innerText = "Auto Size"
+    top.document.body.appendChild(Button2);
+top.document.body.appendChild(menu);
+*/
+/*
+wight
+lenght
+`*/
